192b179870d1997aeb25c5026489f1fad39ce164
luciano <luchoserra288@gmail.com>
2025-06-04 22:45:34 -0300

    Fix gráfico para shoots.

--- client/bullet_view.h
+++ client/bullet_view.h
@@ -10,6 +10,9 @@
 
 #include "game_camera.h"
 
+#define BULLET_WIDTH 4
+#define BULLET_HEIGHT 4
+
 class BulletView {
 private:
     // int id;
@@ -26,8 +29,8 @@ public:
     }
 
     void draw(SDL2pp::Renderer& renderer, const GameCamera& camera) {
-        SDL2pp::Rect rect = {static_cast<int>(x - camera.get_x()),
-                             static_cast<int>(y - camera.get_y()), 8, 8};
+        SDL2pp::Rect rect = {static_cast<int>(x - camera.get_x()) + BULLET_WIDTH / 2,
+                             static_cast<int>(y - camera.get_y()) + BULLET_HEIGHT / 2, BULLET_WIDTH, BULLET_HEIGHT};
         renderer.SetDrawColor(0, 0, 0, 1);
         renderer.FillRect(rect);
     }
